b4a8ec0fc364f321f8d9839bcd7d839e93ce8f30
euclidesroario <51226601+euclidesrosario@users.noreply.github.com>
2023-08-03 16:49:21 +0200

    Update WebAppApplication.java

--- web-app/web-app/src/main/java/com/challenge/webapp/WebAppApplication.java
+++ web-app/web-app/src/main/java/com/challenge/webapp/WebAppApplication.java
@@ -1,13 +1,27 @@
 package com.challenge.webapp;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.challenge.webapp.controller.TravelInfoController;
+
 
 @SpringBootApplication
 public class WebAppApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebAppApplication.class, args);
+		
+//TravelInfoController.getCountryCurrency();
+		
+		
+	    }
+
+	    
+		
 	}
 
-}
+
